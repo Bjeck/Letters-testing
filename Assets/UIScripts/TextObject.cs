c67a6d5bfe1713ca75405c6c4369cbfc67b2580f
Bjeck <bjarke@larsen-dk.dk>
2016-10-12 15:37:59 +0200

    moving

---
+++ Assets/UIScripts/TextObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TextObject : UIOBject {
+
+	public TextContent a,b;
+	public List<List<string>> strings = new List<List<string>> ();
+
+//	public string textString;
+
+}
